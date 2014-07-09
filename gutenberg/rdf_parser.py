df75e838e80aeb03026e3a2372596fd7331fc3f7
Rashiq <rashiq.z@gmail.com>
2014-07-09 19:15:27 +0200

    Wrapped the Parser in a class and added documentation

--- gutenberg/rdf_parser.py
+++ gutenberg/rdf_parser.py
@@ -1,49 +1,58 @@
 from bs4 import BeautifulSoup
+import requests 
 
-def read_xml(name):
+
+class RdfParser():
+
+    def __init__(self, html):
+            self.html = html
+
+    def parse(self):
+        soup = BeautifulSoup(self.html)
+
+        # Parsing the name. Sometimes it's the name of 
+        # an organization or the name is not known and therefore
+        # the <dcterms:creator> or <marcrel:com> node only return 
+        # "anonymous" or "unknown". For the case that it's only one word
+        # `self.laste_name` will be null.
+        # Because of a rare edge case that the fild of the parsed author's name 
+        # has more than one comma we will join the first name in reverse, starting
+        # with the second item.
+        self.first_name = ''
+        self.last_name = ''
+        self.author = soup.find('dcterms:creator')
+        if not self.author:
+            self.author = soup.find('marcrel:com')
+        self.author = self.author.find('pgterms:name').text
+        self.author_name = self.author.split(',')
+        self.first_name = ''.join(self.author.split(',')[::-1])
+        self.last_name = self.author.split(',')[0]
+
+        # Parsing the birth and (death, if the case) date of the author.
+        # These values are likely to be null.
+        self.birth_date = soup.find('pgterms:birthhdate')
+        self.birth_date = self.birth_date.text if self.birth_date else None
+        self.death_date = soup.find('pgterms:deathhdate')
+        self.death_date = self.death_date.text if self.death_date else None
+        
+        # ISO 639-1 language codes that consist of 2 letters 
+        self.language =  soup.find('dcterms:language').find('rdf:value').text
+        # The download count of the books on www.gutenberg.org.
+        # This will be used to determine the popularity of the book.
+        self.downloads = soup.find('pgterms:downloads').text 
+        # The book might be licensed under GPL, public domain
+        # or might be copyrighted
+        self.license = soup.find('dcterms:rights').text
+
+        return self
+
+
+
+if __name__ == '__main__':
+    # Bacic Test with a sample rdf file
     data = ''
-    with open('pg'+name+'.rdf', 'r') as f:
+    with open('pg45213.rdf', 'r') as f:
         data = f.read()
-    
-    soup = BeautifulSoup(data)
-    
-    # Basic identification
-    book_id = name
-    title = soup.find('dcterms:title').text
-    subtitle = ' '.join(title.split('\n')[1:])
-    title = title.split('\n')[0]
-    
-    # Author
-    first_name = ''
-    last_name = ''
-    author = author = soup.find('dcterms:creator')
-    if author == None:
-        author = soup.find('marcrel:com')
-    author = author.find('pgterms:name').text
-    author_name = author.split(',')
-    if author == 'Anonymous':
-        print 'Anonymous author' # TODO
-    elif author == 'Various':
-        print 'Various author' # TODO
-    elif len(author_name) == 2:
-        first_name = author.split(',')[1]
-        last_name = author.split(',')[0]
-    birth_date = soup.find('pgterms:birthhdate')
-    birth_date = birth_date.text if birth_date else None
-    death_date = soup.find('pgterms:deathhdate')
-    death_date = death_date.text if death_date else None
-    
-    # Other basic metadata
-    language =  soup.find('dcterms:language').find('rdf:value').text
-    downloads = soup.find('pgterms:downloads').text 
-    license = soup.find('dcterms:rights').text
-    
-    print 'book id='+book_id
-    print 'title='+title
-    print 'subtitle='+subtitle
-    print 'author='+author
-    
 
-
-read_xml('45213')
-
+    parser = RdfParser(data).parse()
+    print vars(parser)
