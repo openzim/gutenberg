e79129138a57ccc87df35280275202c095b7bdb7
Seb35 <seb35wikipedia@gmail.com>
2014-07-09 18:27:46 +0200

    Added a function around the reading in rdf_parser.py

--- gutenberg/rdf_parser.py
+++ gutenberg/rdf_parser.py
@@ -1,21 +1,25 @@
 from bs4 import BeautifulSoup
 
-data = ''
-with open('pg45213.rdf', 'r') as f:
-	data = f.read()
+def read_xml(name):
+    data = ''
+    with open('pg'+name+'.rdf', 'r') as f:
+        data = f.read()
+    
+    soup = BeautifulSoup(data)
+    
+    book_id = name
+    title = soup.find('dcterms:title').text
+    subtitle = ''.join(title.split('\n')[1:])
+    author = soup.find('dcterms:creator').find('pgterms:name').text
+    first_name = author.split(',')[1]
+    last_name = author.split(',')[0]
+    license = soup.find('dcterms:rights').text
+    language =  soup.find('dcterms:language').find('rdf:value').text
+    downloads = soup.find('pgterms:downloads').text 
+    brith_date = soup.find('pgterms:birthhdate')
+    brith_date = brith_date.text if brith_date else None
+    death_date = soup.find('pgterms:deathhdate')
+    death_date = death_date.text if death_date else None
 
-soup = BeautifulSoup(data)
-
-title = soup.find('dcterms:title').text
-subtitle = ''.join(title.split('\n')[1:])
-author = soup.find('dcterms:creator').find('pgterms:name').text
-first_name = author.split(',')[1]
-last_name = author.split(',')[0]
-license = soup.find('dcterms:rights').text
-language =  soup.find('dcterms:language').find('rdf:value').text
-downloads = soup.find('pgterms:downloads').text 
-brith_date = soup.find('pgterms:birthhdate')
-brith_date = brith_date.text if brith_date else None
-death_date = soup.find('pgterms:deathhdate')
-death_date = death_date.text if death_date else None
+read_xml('45213')
 
