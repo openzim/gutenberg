9fa0ddfb299251e7d2f4d70287f69a6261c051f9
renaud gaudin <rgaudin@gmail.com>
2014-07-09 21:41:25 +0200

    skel for dl

---
+++ gutenberg/download.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+
+from gutenberg import logger
+
+
+def download_all_books():
+
+    return
