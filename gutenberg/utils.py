75951e615c6c8c8d4c6f315a081c977f9d089b72
Rashiq <rashiq.z@gmail.com>
2014-07-10 16:12:49 +0200

    Added an UrlBuilder that will generate the url of the book files

--- gutenberg/utils.py
+++ gutenberg/utils.py
@@ -6,10 +6,56 @@ from __future__ import (unicode_literals, absolute_import,
                         division, print_function)
 import logging
 
-import envoy
+from os import path
+
+#import envoy
 
 logger = logging.getLogger(__name__)
 
 
 def exec_cmd(cmd):
     return envoy.run(str(cmd))
+
+
+class UrlBuilder:
+
+    """
+    Url builder for the files of a Gutenberg book.
+    """
+    base = 'http://zimfarm.kiwix.org/gutenberg/'
+
+    def __init__(self):
+        pass
+
+    def build(self):
+        if self.id > 10:
+            base_url = path.join(path.join(*list(str(self.id))[:-1]), str(self.id))
+            base_url = path.join(self.base, base_url)
+            urls = [path.join(base_url, x) for x in self.file_types]
+        else:
+            logger.warning('Figuring out the url of books \
+                with an ID of {ID <= 10} is not implemented yet')
+            return None
+
+        return urls
+
+    def with_base(self, base):
+        self.base = base
+
+    def with_id(self, id):
+        self.id = id
+
+
+    def with_files(self, file_types):
+        self.file_types = file_types
+
+    def __unicode__(self):
+        return self.build_url()
+
+
+if __name__ == '__main__':
+    b = UrlBuilder()
+    b.with_id(1234)
+    b.with_files([u'1234.kindle.noimages', u'1234-8.txt', u'1234.kindle.images', 
+        u'1234-h.zip', u'1234.epub.images', u'1234.epub.noimages'])
+    print('Url of the book: ' + str(b.build()))
