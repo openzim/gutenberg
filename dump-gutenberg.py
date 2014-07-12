e9b26fd3633d9731d6a09c01f21845f448dcc80f
renaud gaudin <rgaudin@gmail.com>
2014-07-12 19:09:03 +0200

    added zim stage

--- dump-gutenberg.py
+++ dump-gutenberg.py
@@ -69,7 +69,7 @@ def main(arguments):
                  for x in (arguments.get('--languages') or '').split(',')
                  if x.strip()]
     # special shortcuts for "all"
-    if arguments.get('--formats') == ['all']:
+    if arguments.get('--formats') == 'all':
         FORMATS = ['epub', 'pdf']
     else:
         FORMATS = [x.strip().lower()
