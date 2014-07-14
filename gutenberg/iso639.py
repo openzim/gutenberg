9f5476401971022d00306752e1218c40eceed4f4
renaud gaudin <rgaudin@gmail.com>
2014-07-14 13:01:14 +0200

    added unicode_literals import to iso file

--- gutenberg/iso639.py
+++ gutenberg/iso639.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
-# -*- coding:  utf-8 -*-
-# vim:  ai ts=4 sts=4 et sw=4 nu
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
 
 import babel
 
