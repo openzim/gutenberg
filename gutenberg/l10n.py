ce62bdd41910e0a269d058748a0ce2f839e9e682
Seb35 <seb35wikipedia@gmail.com>
2014-07-15 04:39:31 +0200

    Add a missing string

--- gutenberg/l10n.py
+++ gutenberg/l10n.py
@@ -22,6 +22,9 @@ l10n_strings = {
       "choose-language": {
         "placeholder": "Choose a language..."
       },
+      "ui-language-switcher": {
+        "textContent": "Interface language"
+      },
       "search": {
         "textContent": "Search"
       },
@@ -143,6 +146,9 @@ l10n_strings = {
       },
       "choose-language": {
         "placeholder":"Choisissez une langue..."
+      },
+      "ui-language-switcher": {
+        "textContent": "Langue d’interface"
       },
       "search": {
         "textContent": "Rechercher"
