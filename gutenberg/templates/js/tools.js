7fce1151ca2960e8b28278fa98e582622b1dcc42
renaud gaudin <rgaudin@gmail.com>
2017-04-06 15:21:08 +0200

    Issue #37: added little spacing between icons on Home

--- gutenberg/templates/js/tools.js
+++ gutenberg/templates/js/tools.js
@@ -261,13 +261,13 @@ function showBooks() {
 								var urlBase = encodeURIComponent( full[0].replace( "/", "-" ).substring(0, 230) );
 
 								if (data[0] == 1) {
-								    html += "<a title=\"" + full[0]+ ": HTML\" href=\"../A/" + urlBase + "." + full[3] + ".html\"><i class=\"fa fa-html5 fa-3x\"></i></a>";
+								    html += "<a class=\"home-icon\" title=\"" + full[0]+ ": HTML\" href=\"../A/" + urlBase + "." + full[3] + ".html\"><i class=\"fa fa-html5 fa-3x\"></i></a>";
 								}
 								if (data[1] == 1) {
-								    html += "<a title=\"" + full[0]+ ": EPUB\" href=\"../I/" + urlBase + "." + full[3] + ".epub\"><i class=\"fa fa-download fa-3x\"></i></a>";
+								    html += "<a class=\"home-icon\" title=\"" + full[0]+ ": EPUB\" href=\"../I/" + urlBase + "." + full[3] + ".epub\"><i class=\"fa fa-download fa-3x\"></i></a>";
 								}
 								if (data[2] == 1) {
-								    html += "<a title=\"" + full[0]+ ": PDF\" href=\"../I/" + urlBase + "." + full[3] + ".pdf\"><i class=\"fa fa-file-pdf-o fa-3x\"></i></a>";
+								    html += "<a class=\"home-icon\" title=\"" + full[0]+ ": PDF\" href=\"../I/" + urlBase + "." + full[3] + ".pdf\"><i class=\"fa fa-file-pdf-o fa-3x\"></i></a>";
 								}
 
 								return html;
