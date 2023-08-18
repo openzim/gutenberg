803224874dd4eeaf22e06792868f77675178b7c2
benoit74 <benoit74@users.noreply.github.com>
2023-08-18 13:24:27 +0200

    Remove useless hack

--- src/gutenberg2zim/__main__.py
+++ src/gutenberg2zim/__main__.py
@@ -1,15 +1,4 @@
-import pathlib
-import sys
-
-
-def main():
-    # allows running it from source using python gutenberg2zim
-    sys.path = [str(pathlib.Path(__file__).parent.parent.resolve()), *sys.path]
-
-    from gutenberg2zim.entrypoint import main as entry
-
-    entry()
-
+from gutenberg2zim.entrypoint import main
 
 if __name__ == "__main__":
     main()
